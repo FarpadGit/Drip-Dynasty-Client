7e2f14bbee1e160c5afeba73d8661dc08617a0e4
Fabók Árpád <farpadmail@gmail.com>
2024-12-18 19:45:13 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,30 @@
 import { Routes } from '@angular/router';
-import { ProductFormComponent } from './components/admin/products/product-form/product-form.component';
-import { AdminLayoutComponent } from './components/layouts/admin-layout.component';
-import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
-import { ProductsComponent as AdminProductsComponent } from './components/admin/products/products.component';
-import { ProductsComponent as CustomerProductsComponent } from './components/customer/product/products.component';
 import { customerLayoutComponent } from './components/layouts/customer-layout.component';
-import { authGuard, authGuardChildren } from './guards/auth.guard';
-import { LoginComponent } from './components/auth/login/login.component';
-import { CustomersComponent } from './components/admin/customers/customers.component';
-import { OrdersComponent as AdminOrdersComponent } from './components/admin/orders/orders.component';
+import { AdminLayoutComponent } from './components/layouts/admin-layout.component';
+import { HomeComponent } from './components/customer/home/home.component';
+import { ProductsComponent as CustomerProductsComponent } from './components/customer/product/products.component';
 import { OrdersComponent as CustomerOrdersComponent } from './components/customer/orders/orders.component';
 import { ProductDetailsComponent } from './components/customer/product/details/details.component';
 import { PurchaseProcessedComponent } from './components/customer/purchase-processed/purchase-processed.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { ProductsComponent as AdminProductsComponent } from './components/admin/products/products.component';
+import { ProductFormComponent } from './components/admin/products/product-form/product-form.component';
+import { CustomersComponent } from './components/admin/customers/customers.component';
+import { OrdersComponent as AdminOrdersComponent } from './components/admin/orders/orders.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { authGuard, authGuardChildren } from './guards/auth.guard';
 import { purchaseGuard } from './guards/purchase.guard';
-import { HomeComponent } from './components/customer/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: customerLayoutComponent,
     children: [
-      { path: '', component: HomeComponent },
+      {
+        path: '',
+        title: 'Drip Dynasty',
+        component: HomeComponent,
+      },
       {
         path: 'products',
         title: 'Drip Dynasty - Browse our catalog',
@@ -47,6 +51,7 @@ export const routes: Routes = [
   },
   {
     path: 'admin',
+    title: 'Drip Dynasty Admin Panel',
     component: AdminLayoutComponent,
     canActivate: [authGuard],
     canActivateChild: [authGuardChildren],
@@ -76,7 +81,7 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    title: 'Drip Dynasty - Admin Panel Login',
+    title: 'Drip Dynasty Admin Panel Login',
     component: LoginComponent,
   },
 ];
