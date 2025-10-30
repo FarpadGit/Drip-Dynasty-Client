9c9cea7fbd808288ca543650393514a6ae0bc6ff
FarpadGit <farpadmail@gmail.com>
2025-10-30 01:30:43 +0100

    Update textarea.directive.ts

--- src/app/directives/UI/textarea.directive.ts
+++ src/app/directives/UI/textarea.directive.ts
@@ -10,5 +10,6 @@ export class TextareaDirective {
     (eleRef.nativeElement as HTMLElement).className =
       'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background resize-none placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ' +
       hostClasses;
+    (eleRef.nativeElement as HTMLTextAreaElement).spellcheck = false;
   }
 }
